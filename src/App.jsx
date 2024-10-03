20f0d2d19d5d5499f46b5dce448489d604826a79
Amaljosen <apostlemichael.india@gmail.com>
2024-10-03 14:11:02 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { useState } from "react";
 
 function App() {
+  const [dance,setDance]=useState(false)
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+    <>
+    <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
-      <Experience />
+      <Experience dance={dance} setDance={setDance}/>
     </Canvas>
+
+
+    <div className="absolute top-5 p-2 text-white">
+    <button className="bg-green-700 text-white px-4 py-2 rounded hover:bg-green-800 transition-colors duration-200" onClick={()=>setDance(true)}>
+     Let's Dance
+    </button>
+    <button className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-800 transition-colors duration-200 ml-2" onClick={()=>setDance(false)}>
+      Stop Dancing
+    </button>
+     </div>
+
+    </> 
+  
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,14 @@
 import { OrbitControls } from "@react-three/drei";
+import { Avatar } from "./Avatar";
 
-export const Experience = () => {
+export const Experience = ({dance,setDance}) => {
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <group position-y={-1}>
+      <Avatar dance={dance} setDance={setDance}/>
+      </group>
+     <ambientLight intensity={2}/>
     </>
   );
 };
